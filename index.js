637efd2e019a3b5fb008e03f2ad51d1763d93349
rpyimuk <prijmuk1711@gmail.com>
2024-08-07 20:48:30 +0300

    created server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+let requestCount = 0;
+const requestLimit = 50;
+const resetInterval = 1000;
+
+setInterval(() => {
+    requestCount = 0;
+}, resetInterval);
+
+app.get('/api', (req, res) => {
+    requestCount++;
+    if (requestCount > requestLimit) {
+        return res.status(429).send('Too Many Requests');
+    }
+
+    const index = parseInt(req.query.index);
+    const delay = Math.floor(Math.random() * 1000) + 1;
+
+    setTimeout(() => {
+        res.json({ index });
+    }, delay);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
